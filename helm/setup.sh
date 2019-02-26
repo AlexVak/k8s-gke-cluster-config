3f8ad3587a5f0ebdb8ea886f078f9af02bda19d7
AlexVak <avakarchuk@me.com>
2019-02-26 16:29:51 +0200

    Update setup.sh

--- helm/setup.sh
+++ helm/setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # https://github.com/helm/helm
 kubectl create -f rbac-config.yaml
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
 helm init --service-account tiller

6f0edbb0d912f5b1b0270a447c37de7747bcb9a5
AlexVak <avakarchuk@me.com>
2019-02-26 16:40:20 +0200

    Update setup.sh

--- cert-manager/setup.sh
+++ cert-manager/setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 # https://github.com/jetstack/cert-manager
-helm install --name cert-manager --namespace kube-system stable/cert-manager
+
+# Install the CustomResourceDefinition resources separately
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+
+# Create the namespace for cert-manager
+kubectl create namespace cert-manager
+
+# Label the cert-manager namespace to disable resource validation
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install --name cert-manager --namespace cert-manager stable/cert-manager
